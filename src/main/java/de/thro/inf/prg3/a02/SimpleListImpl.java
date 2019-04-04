03b2874e0a4272d3f7b8cb2350843e53792e4416
easyserdar <easyserdar@gmail.com>
2019-04-04 11:10:38 +0200

    Fast endgültige Lösung

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,60 +1,53 @@
 package de.thro.inf.prg3.a02;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 
-/**
- * @author Peter Kurfer
- * Created on 10/6/17.
- */
 public class SimpleListImpl implements SimpleList, Iterable {
 
     Element head;
-
-
-    private static class SimpleIteratorImpl implements Iterator {
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public Object next() {
-            return null;
-        }
-    }
+    int size = 0;
 
     private static class Element {
         Object item;
         Element next;
+        Element(Object o) { item = 0; }
     }
 
     @Override
-    public void add(Object item) {
+    public void add(Object o) {
+        size++;
+
         if (this.head == null) {
-            this.head = (Element) item;
-        }
-        else {
-            for ( Iterator i = list.iterator(); i.hasNext(); )
-            {
-                Object o = i.next();
-            }
-            this.head.next = (Element) item;
+            this.head = new Element(o);
+            return;
         }
 
+        Element it = head;
+        while (it.next != null) {
+            it = it.next;
+        }
 
+        it.next = new Element(o);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public SimpleList filter(SimpleFilter filter) {
-        return null;
+        SimpleList sl = new SimpleListImpl();
+        for (Object o : this) {
+            if (filter.include(o)) {
+                sl.add(o);
+            }
+        }
+
+        return sl;
     }
 
+    //muss noch richtig gestellt werden
     @Override
     public boolean include(Object item) {
         return false;
@@ -62,9 +55,20 @@ public class SimpleListImpl implements SimpleList, Iterable {
 
     @Override
     public Iterator iterator() {
-        return null;
+        return new Iterator() {
+            Element current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Object next() {
+                Object o = current.item;
+                current = current.next;
+                return o;
+            }
+        };
     }
-
-    // TODO: Implement the required methods.
-
 }
